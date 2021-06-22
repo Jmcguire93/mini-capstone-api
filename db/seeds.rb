92e245d6a8cfadf080509a52ef839a178dc07dd7
James <james.ryan.mcguire@gmail.com>
2021-06-22 15:19:35 -0700

    Adds seed dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Computer", price: 800, image_url: "https://images-na.ssl-images-amazon.com/images/I/7161%2BB-5StL._AC_SL1500_.jpg", description: "This is a description of the Computer product.")
+# Product.create(name: "Computer", price: 800, image_url: "https://images-na.ssl-images-amazon.com/images/I/7161%2BB-5StL._AC_SL1500_.jpg", description: "This is a description of the Computer product.")
 
-Product.create(name: "Keyboard", price: 50, image_url: "https://mercari-images.global.ssl.fastly.net/photos/m99461935228_1.jpg?1598297530", description: "This is a description of the Keyboard product.")
+# Product.create(name: "Keyboard", price: 50, image_url: "https://mercari-images.global.ssl.fastly.net/photos/m99461935228_1.jpg?1598297530", description: "This is a description of the Keyboard product.")
