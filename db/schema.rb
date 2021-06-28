3a0c8224256cb1cb3e5dea75a63a4ef7ef03435b
James <james.ryan.mcguire@gmail.com>
2021-06-28 13:31:44 -0700

    Adds user model and bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_211322) do
+ActiveRecord::Schema.define(version: 2021_06_28_203054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2021_06_25_211322) do
   end
 
   create_table "untitled_table", id: :serial, force: :cascade do |t|
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
