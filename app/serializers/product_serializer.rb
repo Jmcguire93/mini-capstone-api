e6601e4244d78e95e153d95ffed133c054283ef3
James <james.ryan.mcguire@gmail.com>
2021-07-02 14:11:52 -0700

    Adds has_many categories to product_serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,5 +2,6 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :image_url, :description, :is_discounted?, :tax, :total, :stock, :supplier_id 
 
   belongs_to :supplier 
+  has_many :categories
 end
   
