ad723796aa89ed9ee790179a87a8bb7110c7e406
James <james.ryan.mcguire@gmail.com>
2021-07-07 15:35:06 -0700

    Adds money_math method to Order model and serializer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,14 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    carted_products = current_user.carted_products.where(status: "Carted")
-   
-    calculated_subtotal = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-    end
-    calculated_tax = calculated_subtotal * 0.9 
-    calculated_total = calculated_subtotal + calculated_tax
+    # carted_products = current_user.carted_products.where(status: "Carted")
+    
+    # calculated_subtotal = 0
+    # carted_products.each do |carted_product|
+    #   calculated_subtotal += carted_product.quantity * carted_product.product.price
+    # end
+    # calculated_tax = calculated_subtotal * 0.9 
+    # calculated_total = calculated_subtotal + calculated_tax
 
     order = Order.new(
       user_id: current_user.id, 
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,20 @@ class Order < ApplicationRecord
   has_many :products, through: :carted_products
   # belongs_to :product
   # has_many :products
+
+  def money_math
+    # carted_products = current_user.carted_products.where(status: "Carted")
+   
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+    calculated_tax = calculated_subtotal * 0.9 
+    calculated_total = calculated_subtotal + calculated_tax
+
+    self.subtotal = calculated_subtotal
+    self.tax = calculated_tax,
+    self.total = calculated_total
+    self.save
+  end
 end
